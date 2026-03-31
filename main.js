e26130d127b74276b8512ed79416c73d46feb246
braee1525-byte <braee1525@gmail.com>
2026-03-31 09:48:55 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const{app, BrowserWindow, ipcMain, BrowserView }=require('electron');
+app.disableHardwareAcceleration();
+
+const path=require('node:path');
+const fs=require('node:fs');
+
+function createWindow(){
+    const win =new BrowserWindow({
+        width:900,
+        height:600,
+        webPreferences:{
+            preload:path.join(__dirname,'preload.js'),
+            contextIsolation:true,
+            nodeIntegration:false
+        }
+    });
+    win.loadFile('index.html');
+}
+app.whenReady().then(()=>{
+    createWindow();
+
+    app.on('activate',()=>{
+        if(BrowserWindow.getAllWindows().length===0) createWindow();
+    });
+});
+
+app.on('window-all-closed',()=>{
+    if(process.platform !=='darwin') app.quit();
+});
+
+ipcMain.handle('save-note', async(event,text)=>{
+    const filePath=path.join(app.getPath('documents'),'quicknote.txt');
+    fs.writeFileSync(filePath,text,'utf-8');
+    return{success:true};
+});
+ipcMain.handle('load-note', async()=>{
+    const filePath=path.join(app.getPath('documents'),'quicknote.txt');
+    if(fs.existsSync(filePath)){
+        return fs.readFileSync(filePath,'utf-8');
+    }
+    return'';
+});
