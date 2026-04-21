5c2a99e33e2a2fc701c493d09b6d63b5b8f1f06d
braee1525-byte <braee1525@gmail.com>
2026-04-21 11:45:58 +0900

    Add files via upload

--- preload.js
+++ preload.js
@@ -1,7 +1,15 @@
-const{contextBridge,ipcRenderer}=require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electronAPI',{
-    saveNote:(text)=>ipcRenderer.invoke('save-note', text),
-    loadNote:() => ipcRenderer.invoke('load-note')
+contextBridge.exposeInMainWorld('electronAPI', {
+  saveNote: (note) => ipcRenderer.invoke('save-note', note),
+
+  loadNote: () => ipcRenderer.invoke('load-note'),
+
+  saveAs: (text) => ipcRenderer.invoke('save-as', text),
+
+  deleteNote: () => ipcRenderer.invoke('delete-note'),
+
+  newNote: () => ipcRenderer.invoke('new-note'),
+
+  openFile: () => ipcRenderer.invoke('open-file')
 });
-
